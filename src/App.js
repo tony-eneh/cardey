dd6cc1f7dbc1a760e06223e8d2b6dc7c467575e8
Tony Eneh <aeneh90@gmail.com>
2020-11-01 10:20:14 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import store from "./store";
+import user from "./images/user.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const {
+      name = ". . .",
+      description = " . . .",
+      likes = " . . .",
+      location = " . . .",
+      profilePic,
+      isLoading = true
+    } = store.getState();
+    return (
+      <div className={`${!isLoading ? "App" : "App App--loading"}`}>
+        <div className="App__header">CARDIE</div>
+        <section className="User__img">
+          {profilePic && <img src={user} alt="user" />}
+        </section>
+
+        {!isLoading && (
+          <section className="User__info">
+            <p>
+              {" "}
+              <span className="faint">I am</span> a {description}
+            </p>
+            <p>
+              {" "}
+              <span className="faint">I like</span> {likes}
+            </p>
+            <p className="User__info__details User__info__divider faint">
+              <span>Name: </span>
+              <span>{name}</span>
+            </p>
+            <p className="User__info__details faint">
+              <span>Location: </span>
+              <span>{location}</span>
+            </p>
+          </section>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+import reducer from "../reducers";
+
+const initialState = {};
+
+const store = createStore(reducer, initialState);
+
+export default store;
